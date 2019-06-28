6df2257c1dcee9b01db37e167b632edb018cb639
Denis Hasanenko <denis.hasanenko@gmail.com>
2019-06-28 23:54:54 +0300

    add flask cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from config import config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
@@ -9,6 +10,7 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
+    CORS(app)
     db.init_app(app)
 
     from .notes import notes
